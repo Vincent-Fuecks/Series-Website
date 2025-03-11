63c11868b490d6c6d44cd731d64737b46c5bb3f9
Vincent Fücks <fuev99@zedat.fu-berlin.de>
2025-03-11 15:49:25 +0100

    Added server with Java backend

---
+++ src/main/java/org/series/website/jakarta/IndexServlet.java
@@ -0,0 +1,14 @@
+package org.series.website.jakarta;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+
+import java.io.IOException;
+
+@WebServlet("/index")
+public class IndexServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/index.html").forward(request, response);
+    }
+}
